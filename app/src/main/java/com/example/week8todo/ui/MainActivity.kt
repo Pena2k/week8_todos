8429c75c5766e38ce9106f883f84ec7c26100361
Pena2k <t2rape00@students.oamk.fi>
2024-10-08 21:03:30 +0300

    Tehtävä valmis

---
+++ app/src/main/java/com/example/week8todo/ui/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.week8todo.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.week8todo.model.Todo
+import com.example.week8todo.ui.theme.Week8TodoTheme
+import com.example.week8todo.viewmodel.TodoViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Week8TodoTheme {
+                TodoScreen()
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun TodoScreen(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<Todo>) {
+    LazyColumn (
+        modifier = Modifier.padding(8.dp)
+    ){
+        items(todos) { todo ->
+            Text(
+                text = todo.title,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
+}
